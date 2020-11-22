c354304eb9b79c13af5b5e52184f60580949db97
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-11-23 02:08:59 +0500

    I am learning netlify functions.

--- src/components/DefaultClient.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default () => {
-    return (
-        <div>
-            This is client default client page
-        </div>
-    );
-}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,8 @@
 import React from 'react';
-import { Link, navigate } from 'gatsby';
-import Header from '../components/Header';
-import Layout from '../components/Layout';
-import Button from '@material-ui/core/Button';
 
 
  export default () => {
-    return (
-      <Layout headerTitle="Index Page Header">
-        <Header text="This is a heading" subtext="The is the sub heading" /> 
-        <p> We can link pages in Gatsby by using Link. </p>  
-        <Link to="/about/"> About </Link> <br /> <br />
-        <Button variant="contained" color="primary" onClick={() => { navigate('/about') }}>
-           Navigate to About Page with button.
-        </Button> <br /> <br />
-        <Link to="/app/">Go to Client Page 1</Link> <br /> <br />
-        <Link to="/app/page1/">Go to Client Page 2</Link>
-      </Layout>  
+    return (   
+      <div> Hello World </div>    
     ) 
  }
