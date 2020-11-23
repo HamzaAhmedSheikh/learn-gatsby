036294f4e65adc885a3ccd7ebecb49077dfd85e0
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-11-23 06:25:33 +0500

    I am working and some errors.

--- functions/add/add.js
+++ functions/add/add.js
@@ -1,23 +1,22 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 
-// const random = require("random-name")
 const faunadb = require('faunadb'),
   q = faunadb.query;
 
-const handler = async (event) => {
-  try {    
-    // const subject = event.queryStringParameters.name || 'World'
-    // const name = random.first();
-    var client = new faunadb.Client({ secret: process.env.FAUNADB_ADMIN_SECRET });
 
-    var result = await client.query(      
-       q.Get(q.Ref( q.Collection('posts'), '282842307521151489' ))
-    );
-        // console.log(result);
-        console.log("all the entries " + result.data)
+const handler = async (event) => {  
+  try {
+    // const subject = event.queryStringParameters.name || 'World'
+    var client = new faunadb.Client({ secret: process.env.FAUNADB_ADMIN_SECRET });
+    var result = await client.query(
+      q.Create(
+        q.Collection('posts'), { data: { name: reqObj.name, age: reqObj.age } } 
+      )
+    )
+     console.log(result);
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `${result.data.title}` }),
+      body: JSON.stringify({ id: `${result.ref.id}` }),
       // // more keys you can return:
       // headers: { "headerName": "headerValue", ... },
       // isBase64Encoded: true,
